505eecb561023096cae95d4a1df31e6e7b519499
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-05-01 22:18:07 +0100

    Commit

--- premake5.lua
+++ premake5.lua
@@ -5,12 +5,25 @@ project "FastNoise2"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files {}
-
+	files {
+		"src/FastNoise/**.h",
+		"src/FastNoise/**.cpp",
+		
+		"src/FastSIMD/Internal/**.h",
+		"src/FastSIMD/Example/Example.h",
+		"src/FastSIMD/**.cpp",
+	}
+	
+	includedirs
+    {
+        "include"
+    }
+		
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
 		staticruntime "On"
+		defines { "FASTNOISE_EXPORT" }
 
 	filter "system:linux"
 		pic "On"
@@ -25,3 +38,6 @@ project "FastNoise2"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+
+	filter "action:vs*"
+		buildoptions { "/arch:AVX512" }
