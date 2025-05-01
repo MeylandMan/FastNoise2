ec44099bad3550c8652ea6576dda3f1ac1f91cd3
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-05-01 19:29:50 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+project "FastNoise2"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
